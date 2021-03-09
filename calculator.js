9875a8558b86c8146b567c3719dbbd581562f276
Rachid-L <rachid-leboukh_student2021@wilder.school>
2021-03-09 14:31:55 +0100

    maj

--- calculator.js
+++ calculator.js
@@ -1,1 +1,7 @@
-console.log('Hello Wolrd')
+let firstValue = prompt("Entrez première valeur") 
+let operator = prompt("Entrez l'opérateur")
+let secondValue = prompt("Entrez seconde valeur")
+
+console.log(firstValue)
+console.log(operator)
+console.log(secondValue)
